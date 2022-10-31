27def6a4b8614dfd072622d4057d8736df9564f3
Hak1it <111969611+Hak1it@users.noreply.github.com>
2022-10-31 21:49:23 +0400

    Update button.js
    Norn em tale gyada vle

--- button.js
+++ button.js
@@ -1,42 +1,46 @@
-myClick();document.querySelector('button').onclick = myClick
+document.querySelector('button').onclick = myClick
 
 function myClick(){
 var x =  document.querySelector('.h1').value;
 var y = document.querySelector('.h2').value;
-var mach = 0
-var nmanText ="";
-var longestMach = 0
-var nman = ''
+var nmanTexts = [];
+var nman = '';
+
+
+
+var x =  document.querySelector('.h1').value;
+var y = document.querySelector('.h2').value;
+var nmanTexts = [];
+var nman = '';
 
 
 for(let i=0;i<=x.length;i++){
    
     for(let j=0;j<y.length;j++){
-       mach=0  
-        
-    
-       while(x[i+ mach]==y[j+mach]){ 
        
-        nman+=x[i+mach];
- mach++;
-     
+       if(x[i]===y[j]){
+        nman += x[i]
+        i ++
+       }
     };
-
-if(mach >longestMach){
-    nmanText=''
-     nmanText+=nman
-    longestMach+=mach
+    nmanTexts.push(nman) 
+    nman = ""
+    j = 0 
 }
 
 
+var longest = nmanTexts.sort(
+    function (a, b) {
+        return b.length - a.length;
+    }
+)
+
+document.querySelector('.out').innerHTML = longest[0]
+    console.log(longest[0]);
+
 }
-if(longestMach==0){
-document.querySelector('.out').innerHTML = nmanText 
-console.log('nman chen')
-}else{
-    document.querySelector('.out').innerHTML = nmanText
-    console.log(nmanText);}
-}}
 
 
-myClick();
+
+
+
